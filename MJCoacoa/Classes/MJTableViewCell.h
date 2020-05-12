6cc2343cfbdde6d894c0c15d0f8ed04d0ef1e89a
mjl <2994355324@qq.com>
2020-05-12 11:37:45 +0800

    修改配置文件

---
+++ MJCoacoa/Classes/MJTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  MJTableViewCell.h
+//  Pods
+//
+//  Created by mjl on 2020/5/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MJCoacoa/Classes/MJTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MJTableViewCell.m
+//  Pods
+//
+//  Created by mjl on 2020/5/12.
+//
+
+#import "MJTableViewCell.h"
+
+@implementation MJTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
